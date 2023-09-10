9cd08948971366afd01dc462c191cad1aceba350
yeua6321 <140486106+yeua6321@users.noreply.github.com>
2023-09-10 16:09:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = process.env.SERVER_IP;
-const port = process.env.SERVER_PORT || 3000; /* 当容器平台分配不规则端口时,此处需修改为分配端口 */
+const port = process.env.SERVER_PORT || 8080; /* 当容器平台分配不规则端口时,此处需修改为分配端口 */
 const express = require("express");
 const app = express();
 var exec = require("child_process").exec;
